b585b27f2ff49c8d109c48c5b359bcdfd70343a3
Chris Laverdiere <cmlaverdiere@gmail.com>
2015-01-17 15:20:30 -0500

    Partial login code.

--- web/pantry.py
+++ web/pantry.py
@@ -1,6 +1,12 @@
 import os, sqlite3
 from flask import Flask, url_for, render_template, request, redirect, session, abort, flash, g
+from flask.ext import login
+from flask.ext.login import LoginManager, login_user
+from flask.ext.security import login_required
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+from wtforms.fields import TextField, PasswordField
+from wtforms.validators import *
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -10,15 +16,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'pantry.db'),
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./pantry.db',
     DEBUG=True,
     SECRET_KEY='pantry key',
 ))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE']
 
 db = SQLAlchemy(app)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,6 +46,24 @@ class User(db.Model):
         self.real_name = real_name
         self.address = address
         self.email = email
+
+    def check_password(self, other_pass):
+        return self.password == other_pass
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __unicode__(self):
+        return self.username
 
     def __repr__(self):
         return '<User {} {} {} {}' \
@@ -63,13 +87,63 @@ class Transaction(db.Model):
         return 'Transaction {} {} {} {}' \
                .format(self.name_from, self.name_to, self.item, self.price)
 
+class LoginForm(Form):
+    username = TextField(validators=[required()])
+    password = PasswordField(validators=[required()])
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        rv = Form.validate(self)
+        if not rv:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+
+        if user is None:
+            self.username.errors.append('Unknown username')
+
+        if not user.check_password(self.password.data):
+            self.username.errors.append('Invalid password')
+            return False
+
+        self.user = user
+        return True
+
+    def get_user(self):
+        return db.session.query(User).filter_by(username=self.username.data).first()
+
 @app.route("/")
 def pantry():
     return render_template('home.html')
 
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route("/examples")
 def examples():
     return render_template('examples.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    flash(form.username.data, 'error')
+    flash(form.password.data, 'error')
+    flash(form.validate(), 'error')
+    user = User.query.filter_by(username=username, password=password)
+
+    if form.validate_on_submit():
+        user = form.get_user()
+        login_user(user)
+        flash("Logged in!")
+        session['user_id'] = form.user.id
+        return redirect(request.args.get("next") or url_for('dashboard'))
+    flash("No! Wrong Password! Fuck!")
+    return render_template("home.html", form=form)
 
 @app.route("/register")
 def register():
@@ -103,9 +177,9 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-# @login_manager.user_loader
-# def load_user(userid):
-#     return User.get(userid)
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
 
 if __name__ == "__main__":
     app.run()
