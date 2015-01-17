1207d99ab69aaeec42785fad1b85cbc269ae0ad8
kevcoxe <kevcoxe@gmail.com>
2015-01-17 04:13:26 -0500

    login
    added login and name

--- HelloTest/HelloTest/ViewController.swift
+++ HelloTest/HelloTest/ViewController.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
+class Person {
+    
+    var name: [String:String]
+    var list = [String:Int]()
+    var location: String?
+    
+    init(firstName: String, lastName: String) {
+        self.name = [firstName:lastName]
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +26,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var people = [Person]()
+    
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBAction func addPerson(sender: AnyObject) {
+//        println("First name:\t\(firstNameField.text)")
+//        println("Last name:\t\(lastNameField.text)")
+        var newPerson = Person(firstName: firstNameField.text, lastName: lastNameField.text)
+        people.append(newPerson)
+        firstNameField.text = ""
+        lastNameField.text = ""
+    }
+    
+    @IBAction func showPeople(sender: AnyObject) {
+        for person in people {
+            println("Name:\t \(person.name)")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
