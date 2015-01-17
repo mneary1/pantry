6819b21d724c994cdcdee1e57d651ee15a673b9d
kevcoxe <kevcoxe@gmail.com>
2015-01-17 12:49:37 -0500

    login stuff

--- ios/Pantry/Pantry/ViewController.swift
+++ ios/Pantry/Pantry/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +17,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var login_username: UITextField!
+    @IBOutlet weak var login_password: UITextField!
+    @IBAction func login_button(sender: AnyObject) {
+        if login_username.text != nil && login_password.text != nil {
+            println("username:\t \(login_username.text)")
+            println("password:\t \(login_password.text)")
+            
+            login_username.text = ""
+            login_password.text = ""
+        } else {
+            login_username.text = "Nope"
+            login_password.text = "Nope"
+        }
+    }
+    
+    @IBAction func to_signin(sender: AnyObject) {
+        let view2 = self.storyboard?.instantiateViewControllerWithIdentifier("view2") as myViewController2
+        self.navigationController?.pushViewController(view2, animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
