09b553878f7cbbc7a2c63a2877d927c2be112d22
kevcoxe <kevcoxe@gmail.com>
2015-01-17 00:10:56 -0500

    First commit

---
+++ HelloTest/HelloTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloTest
+//
+//  Created by Kevin Coxe on 1/16/15.
+//  Copyright (c) 2015 Kevin Coxe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
