624ae9dc4a09cfd9099221bf1762511e46c97884
kevcoxe <kevcoxe@gmail.com>
2015-01-17 04:32:32 -0500

    Initial Commit

---
+++ ios/Pantry/Pantry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pantry
+//
+//  Created by Kevin Coxe on 1/17/15.
+//  Copyright (c) 2015 Randomly Generated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
