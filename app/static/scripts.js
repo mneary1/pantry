bfe40a360a2e5fca69ac8a3906be3767dd11a820
Michael Neary <mneary1@umbc.edu>
2015-01-20 22:23:20 -0500

    moves templates and static directories into app because shit broke, oops

---
+++ app/static/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.add-btn-pantry').click(function () {
+    $('#add-food-pantry').removeClass("hidden");
+  });
+  $('.add-btn-sl').click(function () {
+    $('#add-food-sl').removeClass("hidden");
+  });
+});
