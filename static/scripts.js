1a2f30fd385a63b784166cd557fbd47ce0a15e9f
Michael Neary <mneary1@umbc.edu>
2015-01-21 21:17:42 -0500

    deletes root static and template directories

--- static/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.add-btn-pantry').click(function () {
-    $('#add-food-pantry').removeClass("hidden");
-  });
-  $('.add-btn-sl').click(function () {
-    $('#add-food-sl').removeClass("hidden");
-  });
-});
